91276b6f9be2def4d0440f64b441c3aad4f9c36a
Goleraan <67887438+Goleraan@users.noreply.github.com>
2023-12-10 21:25:03 +0100

    Added image extraction

--- src/fitzdoc.py
+++ src/fitzdoc.py
@@ -1,16 +1,23 @@
 """
  pathlib to access PDF files (in pdffiles)
+ os for image output
  logging for logging and debugging
  re for analyzing text with regular expressions
  collections for finding unique items of lists
  fitz from pymupdf to process PDF documents
+ fitzpage to handle individual PDF pages
+ config to use the global configuration
+ outfile for image output
 """
 from pathlib import Path
+import os
 import logging
 import re
 from collections import Counter
 import fitz
 from fitzpage import Fitzpage
+from config import Config
+from outfile import Outfile
 
 class Fitzdoc():
     def __init__(self, file:Path):
@@ -27,6 +34,7 @@ class Fitzdoc():
         self.repeating_text_to_remove = []
         self.html = ''
         self.text = ''
+        self.config = Config()
 
     def check_encryption(self):
         """
@@ -205,3 +213,151 @@ class Fitzdoc():
             if num > detection_threshold:
                 repeating_paragraphs.append((paragraph, num))
         return repeating_paragraphs
+
+    def extract_images(self):
+        """Extract images from a PDF document and write them to the output directory.
+        Inspired by https://github.com/pymupdf/PyMuPDF-Utilities/blob/master/examples/extract-images/extract-from-xref.py
+        License: GNU GPL V3
+        (c) 2018 Jorj X. McKie
+        
+        Some parts are rewritten for the purpose of this method.
+        """
+        # self.status_output = False
+        output_dir = Outfile(self.file).location
+        # if self.status_output:
+        #     print('Starting image extraction for file', self.doc.name)
+        #
+        xref_count = self.doc.xref_length()
+        softmasks = set()
+        img_count = 0
+        total_img_count = 0
+        softmask_count = 0
+        recover_count = 0
+        remove_count = 0
+        #
+        if not Path(output_dir).exists():
+            Path(output_dir).mkdir(parents=True)
+            # if self.status_output:
+            #     print('Created output directory', str(Path(output_dir)))
+        #
+        # Loop over all cross references of the document
+        for xref in range(1, xref_count):
+            try:
+                if self.doc.xref_get_key(xref, 'Subtype')[1] != '/Image':
+                    # Skip all cross references that are not images
+                    continue
+            except RuntimeError as e:
+                print('Error during image extraction\n')
+                print(e)
+                continue
+
+            total_img_count += 1
+
+            if xref in softmasks:
+                # Skip all cross references that are soft masks
+                softmask_count += 1
+                continue
+
+            imgdict = self.doc.extract_image(xref)
+            if not imgdict:
+                # Skip all cross references of type image that are broken
+                continue
+
+            softmask = imgdict['smask']  # reference to a linked soft mask
+            if softmask > 0:
+                # Remember that there is a soft mask, could be referenced multiple times
+                softmasks.add(softmask)
+            #
+            # Outsource some properties of the image dictionary for descriptive access
+            extension = imgdict['ext']
+            imgdata = imgdict['image']
+            width = imgdict['width']
+            height = imgdict['height']
+            imgsize = len(imgdata)
+            #
+            if width <= self.config.co.fitz.images.image_dimension_x_min or\
+               height <= self.config.co.fitz.images.image_dimension_y_min:
+                # Skip image if an edge is too small
+                continue
+            if imgsize < self.config.co.fitz.images.image_size_min:
+                # Skip image if its total file size is too small
+                continue
+            #
+            # Recover the image transparency if there is a soft mask
+            if softmask > 0:
+                imgdict = self.recover_picture(self.doc, imgdict)
+                if imgdict is None:
+                    # Something went wrong, skip image
+                    continue
+                recover_count += 1
+                extension = 'png'  # change file extension to png to consider transparency
+                # Overwrite image data with recovered data that includes alpha channel
+                imgdata = imgdict['image']
+                samplesize = width * height * 3
+                imgsize = len(imgdata)
+            else:
+                # There is no soft mask, no recovery needed
+                samplesize = width * height * max(1, imgdict['colorspace'])
+            #
+            if imgsize / samplesize <= self.config.co.fitz.images.compression_limit:
+                # Skip image if it's compressed to less than 5% (compression_limit) of its full size
+                # These are typically unicolor images that are of no interest
+                continue
+            #
+            # Write image
+            imgname = str(xref) + '.' + extension
+            with open(Path(output_dir, imgname), 'wb') as fp:
+                fp.write(imgdata)
+            img_count += 1
+        #
+        # Remove all soft masks that were written as image file because they slipped
+        # through the previous filter process
+        if len(softmasks) > 0:
+            filelist = os.listdir(output_dir)
+            for softmask in softmasks:
+                img = str(softmask)
+                for fp in filelist:
+                    if fp.startswith(img) and Path(output_dir, fp).exists():
+                        Path(output_dir, fp).unlink()
+                        remove_count += 1
+        #
+        # if self.status_output:
+        #     print('Detected cross references:', xref_count)
+        #     print('Detected images:', total_img_count)
+        #     print('Detected soft mask images:', softmask_count)
+        #     print('Detected relevant images:', img_count)
+        #     print('Recovered transparency with soft masks:', recover_count)
+        #     print('Cleanup of slipped soft masks:', remove_count)
+        #     print('Finished image extraction for', self.doc.name)
+
+    def recover_picture(self, doc:fitz.Document, imgdict):
+        """Code from: https://github.com/pymupdf/PyMuPDF-Utilities/blob/master/examples/extract-images/extract-from-xref.py
+        GNU GPL V3
+        (c) 2018 Jorj X. McKie
+        
+        Minor adjustment: removed unnecessary argument of cross reference.
+        
+        Recovers the pixelmap for a provided image dictionary by applying the soft mask
+        (alpha channel) and making a single data structure out of it that it can be
+        saved as PNG.
+
+        Args:
+            doc (fitz.Document): The PyMuPDF object for a PDF file.
+            imgdict (dict): Dictionary for an image extracted from doc.
+
+        Returns:
+            dict: Updated dictionary for an image that includes the alpha channel information.
+        """
+        smask = imgdict['smask']
+
+        try:
+            pix0 = fitz.Pixmap(imgdict['image'])
+            mask = fitz.Pixmap(doc.extract_image(smask)['image'])
+            pix = fitz.Pixmap(pix0, mask)
+            if pix0.n > 3:
+                ext = 'pam'
+            else:
+                ext = 'png'
+            return {'ext': ext, 'colorspace': pix.colorspace.n, 'image': pix.tobytes(ext)}
+        except:
+            return None
